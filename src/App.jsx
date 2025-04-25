ecf1ca9e38c04e963e031a49d666dd0e3b591023
AilenFlores <ailen.flores@est.fi.uncoma.edu.ar>
2025-04-25 19:21:32 -0300

    Instalacion y configuracion de Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
+       <link href="/src/styles.css" rel="stylesheet"></link>
       <header className="App-header">
-        <h1>Welcome to React</h1>
+      
+      <h1 class="text-3xl font-bold underline">TP REACT !</h1> 
         <p>
           a laburar
         </p>
