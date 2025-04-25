166f86a8a65f32696586d74479fc2b1221fb651a
AilenFlores <ailen.flores@est.fi.uncoma.edu.ar>
2025-04-25 12:33:16 -0300

    Initialize tp-react-2 project with Vite, React, and ESLint setup; add basic App component and styles.

---
+++ tp-react-2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          a laburar
+        </p>
+      </header>
+      </div>
+  )
+}
+
+export default App
