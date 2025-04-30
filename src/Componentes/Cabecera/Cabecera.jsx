a06c595414e2ee6324cfe7fcea860259937342ba
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2025-04-30 12:29:54 -0300

    cabecera

--- src/Componentes/Cabecera/Cabecera.jsx
+++ src/Componentes/Cabecera/Cabecera.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { FaBookmark, FaSearch } from "react-icons/fa";
 
-const Cabecera = () => {
+const Cabecera =() => {
   return (
-    <div className="relative w-full h-32 border-blue-500 md:border-green-500 border-4">
-      <div className="text-red-500 p-4">
-        <a href="#home">LOGO/Inicio</a>      
+    <header className="flex items-center justify-between px-4 py-6 border shadow bg-white">
+      {/* Logo */}
+      <div className="text-red-600 text-2xl font-bold tracking-wider" style={{ transform: "scaleX(1.1)" }}>
+        NERDFLIX
       </div>
-      <div className="absolute bottom-0 left-0 p-4">
-        <input type="text" placeholder="Buscar..." className="border-2 rounded-xl px-2 py-1"/>  
-      </div>
-    </div>
-  );
-};
 
+      {/* Iconos */}
+      <div className="flex items-center space-x-4">
+        <FaBookmark className="text-2xl cursor-pointer" />
+        <button className="px-2 py-1 border rounded text-sm">EN</button>
+        <FaSearch className="text-2xl cursor-pointer" />
+      </div>
+    </header>
+  );
+}
 export default Cabecera;
