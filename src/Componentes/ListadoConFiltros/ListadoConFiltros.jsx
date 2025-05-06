5db7d4f0069219caf70711e988f7d6e7d92fa200
AilenFlores <ailen.flores@est.fi.uncoma.edu.ar>
2025-05-05 21:04:31 -0300

    Ajustar estilos y estructura en ListadoConFiltros y Favoritos para mejorar la presentación y la funcionalidad de carga de favoritos.

--- src/Componentes/ListadoConFiltros/ListadoConFiltros.jsx
+++ src/Componentes/ListadoConFiltros/ListadoConFiltros.jsx
@@ -5,17 +5,19 @@ import Filtro from '../../Componentes/Filtros/Filtros';
 
 const ListadoConFiltros = ({ titulo, contenido, onFiltrar, tipo, cargarMas }) => {
   return (
-    <div className="grid md:grid-cols-[250px_1fr] max-w-screen-xl w-full mx-auto py-3">
-      <Filtro onFiltrar={onFiltrar} tipo={tipo} />
-      <div className="overflow-hidden ml-8 mr-8">
-        <Lista
-          texto={titulo}
-          peliculas={contenido}
-          cargarMas={cargarMas}
-          mensajeCartel="No se encontraron resultados"
-        />
-      </div>
-    </div>
+   <div className="grid md:grid-cols-[250px_1fr] gap-8 max-w-screen-xl w-full mx-auto py-3 mt-8">
+  <Filtro onFiltrar={onFiltrar} tipo={tipo} />
+  <div className="w-full  overflow-hidden"> 
+  <Lista
+    texto={titulo}
+    peliculas={contenido}
+    cargarMas={cargarMas}
+    mensajeCartel="No se encontraron resultados"
+  />
+</div>
+
+</div>
+
   );
 };
 
--- src/Paginas/Favoritos/Favoritos.jsx
+++ src/Paginas/Favoritos/Favoritos.jsx
@@ -5,6 +5,8 @@ import Lista from '../../Componentes/Lista/Lista';
 
 const Favoritos = () => {
   const [peliculas, setPeliculas] = useState([]);
+  const [visibles, setVisibles] = useState(20); 
+
   useEffect(() => {
     const cargarFavoritos = () => {
       const favs = localStorage.getItem('favoritos');
@@ -25,29 +27,33 @@ const Favoritos = () => {
     const handleUpdate = () => {
       cargarFavoritos();
     };
-  
     window.addEventListener('favoritosActualizados', handleUpdate);
   
     return () => {
       window.removeEventListener('favoritosActualizados', handleUpdate);
     };
   }, []);
-  
+
+  const cargarMas = () => setVisibles(prev => prev + 6);
+
   return (
     <div>
-    <Cabecera /> 
-    <div className="bg-gradient-to-b from-white via-purple-800 to-purple-800 min-h-screen p-5 md:p-10">
-    <Lista
-  texto="Películas y series Favoritas"
-  peliculas={peliculas}
-  mensajeCartel={ <>
-      Aún no tienes favoritos guardados.<br />
-      Explorá películas y series y agregalas a tu lista para verlas más tarde.
-    </>
-  }/>   
-    </div> 
-    <Pie />
-  </div>
+      <Cabecera /> 
+      <div className="bg-gradient-to-b from-white via-purple-800 to-purple-800 min-h-screen p-5 md:p-10">
+        <Lista
+          texto="Películas y series Favoritas"
+          peliculas={peliculas.slice(0, visibles)}
+          mensajeCartel={
+            <>
+              Aún no tienes favoritos guardados.<br />
+              Explorá películas o series y agregalas a tu lista para verlas más tarde.
+            </>
+          }
+          cargarMas={peliculas.length > visibles ? cargarMas : null} 
+        />
+      </div> 
+      <Pie />
+    </div>
   );
 };
 
