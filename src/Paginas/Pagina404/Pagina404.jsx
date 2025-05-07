ef23a11d856340938d577b5d25b9303b7222ec15
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2025-05-07 07:58:19 -0300

    traduccion de pagina 404

--- src/Paginas/Pagina404/Pagina404.jsx
+++ src/Paginas/Pagina404/Pagina404.jsx
@@ -3,8 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import Boton from '../../Componentes/Boton/Boton';
 import Cabecera from '../../Componentes/Cabecera/Cabecera'; 
 import Pie from '../../Componentes/Pie/Pie';
+import { useTranslation } from 'react-i18next';
 
 const Pagina404 = () => {
+  const { t } = useTranslation("varios");
+  
   const navigate = useNavigate();
 
   return (
@@ -17,9 +20,9 @@ const Pagina404 = () => {
                 className="w-72 md:w-96 mb-6"
             />
             <h1 className="text-6xl font-bold text-purple-600 mb-4">404</h1>
-            <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-2">Página no encontrada</h2>
-            <p className="text-gray-600 mb-6">Lo sentimos, no pudimos encontrar lo que estás buscando.</p>
-            <Boton texto="Volver al inicio" onClick={() => navigate('/')} className="bg-black hover:bg-red-600" />
+            <h2 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-2">{t("error404.mensaje1")}</h2>
+            <p className="text-gray-600 mb-6">{t("error404.mensaje2")}</p>
+            <Boton texto={t("error404.volver")} onClick={() => navigate('/')} className="bg-black hover:bg-red-600" />
         </div>
       <Pie />
     </div>
